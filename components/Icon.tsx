e407d423ac9b4a1af42f82c39d328d80366d6982
bdowneer191 <bdowneer.bd1@yahoo.com>
2025-07-20 03:02:08 +0600

    Update Icon.tsx

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 
 const icons = {
@@ -54,6 +55,11 @@ const icons = {
   lock: (
      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
       <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 0 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H4.5a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
+    </svg>
+  ),
+  play: (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+        <path d="M8 5v14l11-7z"/>
     </svg>
   ),
   github: (
